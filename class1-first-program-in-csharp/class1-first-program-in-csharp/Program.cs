98de749d40789c9822e8536d1206b433f62afb4f
Vitor Garcia <vitor.stabile@hotmail.com>
2020-12-26 20:50:01 -0300

    class 1 push

---
+++ class1-first-program-in-csharp/class1-first-program-in-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace class1_first_program_in_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
